da3a3686464ecec2bba41c5ea4a3ab7e9372fd09
Turasov Mikhail <miheytm@mail.ru>
2023-04-21 09:47:32 +0500

    Решили задачу 28

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 
 # Задача 26:
-'''
+
 def sqr_ab (a, b):
     if b == 0:
         return 1
     return a * sqr_ab(a, b - 1)
-x = int(input("Введите a: "))
-n = int(input("Введите b: "))
-print(sqr_ab(x, n))
-'''
+x1 = int(input("Введите a: "))
+n1 = int(input("Введите b: "))
+print("a в степени b = ",sqr_ab(x1, n1))
 
+# Задача 28
+def sum_ab (a, b):
+    if b == 0:
+        return a
+    return sum_ab(a + 1, b - 1)
+
+x2 = int(input("Введите a: "))
+n2 = int(input("Введите b: "))
+print("a + b = ", sum_ab(x2, n2))
