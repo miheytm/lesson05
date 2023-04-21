040c466ba21c3b62b77891f04bc8b55bd5ae0ed4
Turasov Mikhail <miheytm@mail.ru>
2023-04-21 09:36:41 +0500

    Решили задачу 26

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# Задача 26:
+'''
+def sqr_ab (a, b):
+    if b == 0:
+        return 1
+    return a * sqr_ab(a, b - 1)
+x = int(input("Введите a: "))
+n = int(input("Введите b: "))
+print(sqr_ab(x, n))
+'''
+
